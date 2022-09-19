da8c573592493591584f822124580a64538c157f
swapnil gawande <gswapnil074@gmail.com>
2022-09-19 19:33:53 +0530

    springBoot with angular sample project

---
+++ springboot/src/main/java/net/backend/springboot/repository/StudentRepository.java
@@ -0,0 +1,10 @@
+package net.backend.springboot.repository;
+
+import net.backend.springboot.model.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface StudentRepository extends JpaRepository<Student, Long>{
+
+}
